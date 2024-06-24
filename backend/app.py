fd121baf1ed99d1e76ccd275d80b726ec2bd63e5
Ritu <ritu31195@gmail.com>
2024-06-24 10:06:32 +0530

    fix: cors error

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,13 @@ from flask_cors import CORS
 from utils.logger import logger
 from routes.weather_route import weather_bp
 
+app = Flask(__name__)
+CORS(app, origins="http://localhost:3000")
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, origins=[
-        "http://localhost:3000",
-    ])
+app.register_blueprint(weather_bp, url_prefix='/api/weather')
 
-    with app.app_context():
-        app.register_blueprint(weather_bp, url_prefix='/api/weather')
 
-    return app
+
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)

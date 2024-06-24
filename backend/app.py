e207eb222e8dd92cdf8a5a94c074b4d71c64a3f0
Ritu <ritu31195@gmail.com>
2024-06-24 10:43:24 +0530

    fix: Handle cors error

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,14 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 app.register_blueprint(weather_bp, url_prefix='/api/weather')
 
+# Define CORS headers after_request
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    return response
+
 
 
 
